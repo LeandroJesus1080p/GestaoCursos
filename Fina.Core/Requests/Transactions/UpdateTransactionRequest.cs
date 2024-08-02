a5b5930f2af95ff35dcac68537e6bf5415bc8355
LeandroJesus1080p <leandro.josus0004@gmail.com>
2024-08-02 15:05:09 -0300

    Finalizado fina.Core

---
+++ Fina.Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,27 @@
+﻿using Fina.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transactions
+{
+    public class UpdateTransactionRequest : Request
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Titulo invalido")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Tipo invalido")]
+        public ETransactionType Type { get; set; } 
+
+        [Required(ErrorMessage = "Valor invalido")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Categoria invalida")]
+        public long CategoryId { get; set; }
+    }
+}
